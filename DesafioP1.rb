5299b5b66a96a8a9c8cec2f6af781d36846d9c69
André Jr <andre3junior@gmail.com>
2024-07-03 17:11:43 -0300

    .

---
+++ DesafioP1.rb
@@ -0,0 +1,6 @@
+puts "Digite o seu nome: "
+nome = gets.chomp
+puts "Digite sua idade"
+idade = gets.chomp
+
+puts "Seu nome eh: " + nome + " e tem " + idade + " anos"
---
+++ hello.rb
@@ -0,0 +1,11 @@
+a=1
+b=2.0
+c="Hello"
+
+puts a.class, b.class, c.class
+
+
+
+h= {curso:"Ruby", idioma:"Port"}
+puts "------------------------------"
+puts h[:curso]
